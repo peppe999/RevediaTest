515e8170c02293de8b505f2e659b5bf05f8645df
FrancescoVengeance <francesco.espositoy@gmail.com>
2021-01-24 00:03:19 +0100

    Added login e signup controller
    Switched login and signup from HTML to JSP

--- src/main/java/it/ingsw/revedia/controller/Login.java
+++ src/main/java/it/ingsw/revedia/controller/Login.java
@@ -4,6 +4,7 @@ import java.sql.SQLException;
 
 import it.ingsw.revedia.utilities.EmailManager;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,22 +15,22 @@ import it.ingsw.revedia.utilities.PasswordManager;
 
 @Controller
 public class Login
-{	
+{
+	@GetMapping("/Login")
+	public String login()
+	{
+		return "login";
+	}
+
 	@PostMapping("/loginUser")
-	public ModelAndView loginUser(@RequestParam("nickname") String nickname, @RequestParam("password") String password)
+	public ModelAndView loginUser(@RequestParam("username") String nickname, @RequestParam("password") String password)
 	{
 		ModelAndView model = new ModelAndView();
 		try
 		{
 			String MD5Password = PasswordManager.getMD5(password);
-			if(DatabaseManager.getIstance().getDaoFactory().getUserJDBC().validateLogin(MD5Password, nickname))
-			{
-				model.setViewName("second.jsp");
-				model.addObject("nickname", nickname);
-			}
-			else
-			{
-				model.setViewName("register.jsp");
+			if(DatabaseManager.getIstance().getDaoFactory().getUserJDBC().validateLogin(MD5Password, nickname)) {
+				model.setViewName("loggato");
 			}
 		} 
 		catch (SQLException e)
---
+++ src/main/java/it/ingsw/revedia/controller/Signup.java
@@ -0,0 +1,58 @@
+package it.ingsw.revedia.controller;
+
+import it.ingsw.revedia.database.DatabaseManager;
+import it.ingsw.revedia.model.User;
+import it.ingsw.revedia.utilities.PasswordManager;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.sql.SQLException;
+
+@Controller
+public class Signup
+{
+    @GetMapping("/signup")
+    public String signup()
+    {
+        return "signup";
+    }
+
+    @PostMapping("/register")
+    public ModelAndView register(HttpServletRequest request, @RequestParam("username") String username,
+                                 @RequestParam("firstname") String firstname, @RequestParam("lastname") String lastname,
+                                 @RequestParam("password") String password, @RequestParam("mail") String mail)
+    {
+        User user = new User();
+        user.setNickname(username);
+        user.setFirstName(firstname);
+        user.setLastName(lastname);
+        user.setMail(mail);
+
+
+        String MD5Password = PasswordManager.getMD5(password);
+        ModelAndView model = new ModelAndView();
+        try
+        {
+            DatabaseManager.getIstance().getDaoFactory().getUserJDBC().insertUser(user, MD5Password);
+            HttpSession session = request.getSession();
+            session.setAttribute("nickname", user.getNickname());
+            session.setAttribute("mail",user.getMail());
+            session.setAttribute("firstname",user.getFirstName());
+            session.setAttribute("lastname",user.getLastName());
+
+            model.setViewName("loggato");
+        }
+        catch (SQLException throwables)
+        {
+            //throwables.printStackTrace();
+            model.setViewName("signup");
+            model.addObject("usernotavaible", "Nome utente non disponibile, riprova");
+        }
+
+        return model;
+    }
+}
