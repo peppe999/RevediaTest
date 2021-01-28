8fc99afdddcf5419b15353f26b8a93781edc6a15
Giuseppe <peri.cle991@gmail.com>
2021-01-28 12:13:31 +0100

    bugfix

--- src/main/java/it/ingsw/revedia/jdbcModels/SongJDBC.java
+++ src/main/java/it/ingsw/revedia/jdbcModels/SongJDBC.java
@@ -95,13 +95,15 @@ public class SongJDBC implements SongDao {
 
 	private Song buildSimplifiedSong(ResultSet result) throws SQLException {
 		String songName = result.getString("songname");
-		int albumID = result.getInt("albumname");
+		String albumName = result.getString("albumname");
+		int albumID = result.getInt("albumid");
 		String user = result.getString("users");
 		float rating = result.getFloat("rating");
 
 		Song song = new Song();
 		song.setName(songName);
 		song.setAlbumID(albumID);
+		song.setAlbumName(albumName);
 		song.setUser(user);
 		song.setRating(rating);
 
@@ -390,7 +392,7 @@ public class SongJDBC implements SongDao {
 	public ArrayList<Song> getHighRateSongs() throws SQLException {
 		Connection connection = this.dataSource.getConnection();
 
-		String query = "select song.name as songname, song.album as albumname, song.users, song.rating from song Order by rating DESC limit 4";
+		String query = "select album.albumid, song.name as songname, album.name as albumname, song.users, song.rating from song inner join album ON album.albumid = song.album Order by song.rating DESC limit 4";
 		PreparedStatement statment = connection.prepareStatement(query);
 		ResultSet result = statment.executeQuery();
 		ArrayList<Song> songs = new ArrayList<Song>();
@@ -415,7 +417,7 @@ public class SongJDBC implements SongDao {
 	public ArrayList<Song> getLatestSongs() throws SQLException {
 		Connection connection = this.dataSource.getConnection();
 
-		String query = "select song.name as songname, song.album as albumname, song.users, song.rating from song Order by postdate DESC limit 3";
+		String query = "select album.albumid, song.name as songname, album.name as albumname, song.users, song.rating from song inner join album ON album.albumid = song.album Order by song.postdate DESC limit 3";
 		PreparedStatement statment = connection.prepareStatement(query);
 		ResultSet result = statment.executeQuery();
 		ArrayList<Song> songs = new ArrayList<Song>();
@@ -470,7 +472,7 @@ public class SongJDBC implements SongDao {
 	public ArrayList<Song> getHighRateSongByGenre(String genre) throws SQLException {
 
 		Connection connection = this.dataSource.getConnection();
-		String query = "select album.albumid, song.name as songname, song.album as albumname, song.users, song.rating from song inner join album on song.album = album.albumid where exists (select * from musical_genre_album where album = albumid and musical_genre = ?) Order by rating DESC limit 4";
+		String query = "select album.albumid, song.name as songname, song.album as albumname, song.users, song.rating from song inner join album on song.album = album.albumid where exists (select * from musical_genre_album where album = albumid and musical_genre = ?) Order by song.rating DESC limit 4";
 		PreparedStatement statment = connection.prepareStatement(query);
 		statment.setString(1, genre);
 		ResultSet result = statment.executeQuery();
